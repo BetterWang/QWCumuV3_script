48eef41e638463b251f5acd883a5a7bf240c5ce5
Quan Wang <BetterWang@gmail.com>
2014-09-16 16:45:24 +0200

    update bGetError.C

--- bGetError.C
+++ bGetError.C
@@ -228,16 +228,29 @@
 	TH1D * fVp[7][4][24];
 	TH1D * fVeta[7][4][24];
 	TH1D * fVc[7][4][2];
+
+
+	TH1D * fcV[7][4];
+	TH1D * fcX[7][4];
+	TH1D * fcVp[7][4][24];
+	TH1D * fcVeta[7][4][24];
+	TH1D * fcVc[7][4][2];
+
 	for ( int n = 1; n < 7; n++ ) {
 		for ( int np = 0; np < 4; np++ ) {
 			fV[n][np] = new TH1D(Form("hV%i%i", n, 2+2*np), "", 20, 0, 20);
 			fX[n][np] = new TH1D(Form("hX%i%i", n, 2+2*np), "", 20, 0, 20);
+			fcV[n][np] = new TH1D(Form("hcV%i%i", n, 2+2*np), "", 20, 0, 20);
+			fcX[n][np] = new TH1D(Form("hcX%i%i", n, 2+2*np), "", 20, 0, 20);
 			for ( int j = 0; j < 24; j++ ) {
 				fVp[n][np][j] = new TH1D(Form("hVp%i%i_%i", n, 2+2*np, j), "", 20, 0, 20);
 				fVeta[n][np][j] = new TH1D(Form("hVeta%i%i_%i", n, 2+2*np, j), "", 20, 0, 20);
+				fcVp[n][np][j] = new TH1D(Form("hcVp%i%i_%i", n, 2+2*np, j), "", 20, 0, 20);
+				fcVeta[n][np][j] = new TH1D(Form("hcVeta%i%i_%i", n, 2+2*np, j), "", 20, 0, 20);
 			}
 			for ( int j = 0; j < 2; j++ ) {
 				fVc[n][np][j] = new TH1D(Form("hVc%i%i_%i", n, 2+2*np, j), "", 20, 0, 20);
+				fcVc[n][np][j] = new TH1D(Form("hcVc%i%i_%i", n, 2+2*np, j), "", 20, 0, 20);
 			}
 		}
 	}
@@ -278,10 +291,17 @@
 				double errX = sqrt( (sumX/s3 - dX[s3][n][np][i]*dX[s3][n][np][i] ) / s3 );
 				fC[n][np]->SetBinError(i+1, errC);
 				fD[n][np]->SetBinError(i+1, errD);
+
 				fV[n][np]->SetBinContent(i+1, dV[s3][n][np][i]);
 				fX[n][np]->SetBinContent(i+1, dX[s3][n][np][i]);
 				fV[n][np]->SetBinError(i+1, errV);
 				fX[n][np]->SetBinError(i+1, errX);
+
+				fcV[n][np]->SetBinContent(i+1, dV[s3][n][np][i]);
+				fcX[n][np]->SetBinContent(i+1, dX[s3][n][np][i]);
+				fcV[n][np]->SetBinError(i+1, dV[s3][n][np][i]/(2+2*np)*errC/dC[s3][n][np][i]);
+				fcX[n][np]->SetBinError(i+1, dX[s3][n][np][i]/(2+2*np)*errD/dD[s3][n][np][i]);
+
 				for ( int fn = 0; fn < s3; fn++ ) {
 					hGausC->Fill((dC[fn][n][np][i] - dC[s3][n][np][i])/errC);
 					hGausD->Fill((dD[fn][n][np][i] - dD[s3][n][np][i])/errD);
@@ -298,16 +318,37 @@
 				for ( j = 0; j < 24; j++ ) {
 					double sumVp = 0;
 					double sumVeta = 0;
+					double sumCp = 0;
+					double sumCeta = 0;
 					for ( int fn = 0; fn < s3; fn++ ) {
 						sumVp += dVp[fn][n][np][j][i]*dVp[fn][n][np][j][i];
 						sumVeta += dVeta[fn][n][np][j][i]*dVeta[fn][n][np][j][i];
+
+						sumCp += dCp[fn][n][np][j][i]*dCp[fn][n][np][j][i];
+						sumCeta += dCeta[fn][n][np][j][i]*dCeta[fn][n][np][j][i];
 					}
 					double errVp = sqrt( (sumVp/s3 - dVp[s3][n][np][j][i]) / s3 );
 					double errVeta = sqrt( (sumVeta/s3 - dVeta[s3][n][np][j][i]) / s3 );
+
+					double errCp = sqrt( (sumCp/s3 - dCp[s3][n][np][j][i]) / s3 );
+					double errCeta = sqrt( (sumCeta/s3 - dCeta[s3][n][np][j][i]) / s3 );
+
 					fVp[n][np][j]->SetBinContent(i+1, dVp[s3][n][np][j][i]);
 					fVeta[n][np][j]->SetBinContent(i+1, dVeta[s3][n][np][j][i]);
 					fVp[n][np][j]->SetBinError(i+1, errVp);
 					fVeta[n][np][j]->SetBinError(i+1, errVeta);
+
+					fcVp[n][np][j]->SetBinContent(i+1, dVp[s3][n][np][j][i]);
+					fcVeta[n][np][j]->SetBinContent(i+1, dVeta[s3][n][np][j][i]);
+					fcVp[n][np][j]->SetBinError(i+1, 
+							sqrt( errCp*errCp/dCp[s3][n][np][j][i]/dCp[s3][n][np][j][i]
+								+ (1+2*np)*(1+2*np)/(2+2*np)/(2+2*np)*errC*errC/dC[s3][n][np][i]/dC[s3][n][np][i])*fabs(dVp[s3][n][np][j][i] )
+							);
+					fcVeta[n][np][j]->SetBinError(i+1, 
+							sqrt( errCeta*errCeta/dCeta[s3][n][np][j][i]/dCeta[s3][n][np][j][i]
+								+ (1+2*np)*(1+2*np)/(2+2*np)/(2+2*np)*errC*errC/dC[s3][n][np][i]/dC[s3][n][np][i])*fabs(dVeta[s3][n][np][j][i] )
+							);
+
 					for ( int fn = 0; fn < s3; fn++ ) {
 						hGausVp->Fill( (dVp[fn][n][np][j][i] - dVp[s3][n][np][j][i]) / errVp );
 						hGausVeta->Fill( (dVeta[fn][n][np][j][i] - dVeta[s3][n][np][j][i]) / errVeta );
@@ -318,12 +359,23 @@
 				/////  
 				for ( j = 0; j < 2; j++ ) {
 					double sumVc = 0;
+					double sumCc = 0;
 					for ( int fn = 0; fn < s3; fn++ ) {
 						sumVc += dVc[fn][n][np][j][i]*dVc[fn][n][np][j][i];
+						sumCc += dCc[fn][n][np][j][i]*dCc[fn][n][np][j][i];
 					}
 					double errVc = sqrt( (sumVc/s3 - dVc[s3][n][np][j][i]) / s3 );
+					double errCc = sqrt( (sumCc/s3 - dCc[s3][n][np][j][i]) / s3 );
+
 					fVc[n][np][j]->SetBinContent(i+1, dVc[s3][n][np][j][i]);
 					fVc[n][np][j]->SetBinError(i+1, errVc);
+
+					fcVc[n][np][j]->SetBinContent(i+1, dVc[s3][n][np][j][i]);
+					fcVc[n][np][j]->SetBinError(i+1, 
+							sqrt( errCc*errCc/dCc[s3][n][np][j][i]/dCc[s3][n][np][j][i]
+								+ (1+2*np)*(1+2*np)/(2+2*np)/(2+2*np)*errC*errC/dC[s3][n][np][i]/dC[s3][n][np][i])*fabs(dVc[s3][n][np][j][i] )
+							);
+
 					for ( int fn = 0; fn < s3; fn++ ) {
 						hGausVc->Fill( (dVc[fn][n][np][j][i] - dVc[s3][n][np][j][i]) / errVc );
 						if ( dVc[fn][n][np][j][i] > 0 ) hGausVc->Fill( (dVc[fn][n][np][j][i] - dVc[s3][n][np][j][i]) / errVc );
@@ -343,12 +395,17 @@
 			fW[n][np]->Write();
 			fD[n][np]->Write();
 			fX[n][np]->Write();
+
 			fCraw[n][np]->Write();
 			fWraw[n][np]->Write();
 			fDraw[n][np]->Write();
 			fXraw[n][np]->Write();
+
 			fV[n][np]->Write();
 			fX[n][np]->Write();
+
+			fcV[n][np]->Write();
+			fcX[n][np]->Write();
 			for ( int i = 0; i < 24; i++ ) {
 				fCp[n][np][i]->Write();
 				fCeta[n][np][i]->Write();
@@ -356,11 +413,14 @@
 				fWeta[n][np][i]->Write();
 				fVp[n][np][i]->Write();
 				fVeta[n][np][i]->Write();
+				fcVp[n][np][i]->Write();
+				fcVeta[n][np][i]->Write();
 			}
 			for ( int i = 0; i < 2; i++ ) {
 				fCc[n][np][i]->Write();
 				fWc[n][np][i]->Write();
 				fVc[n][np][i]->Write();
+				fcVc[n][np][i]->Write();
 			}
 		}
 	}
