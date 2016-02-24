6d4a7a8194b55f4ab9f7f3dc8e0a2e80eb3fdb10
Quan Wang <BetterWang@gmail.com>
2016-02-24 18:35:02 +0100

    [+] label

--- label.h
+++ label.h
@@ -13,6 +13,7 @@ char const * fname[] = {
 	"../PbPb2015/HIMinimumBias3/crab_HIMB3_cal_noeff_v2/160216_221440/0000/",			// 10 PbPb MB3 Skim |eta|<1, calo, no eff, 0.3 < pt_ref < 3.0
 	"../PbPb2015/HIMinimumBias4/crab_HIMB4_cal_noeff_v3/160217_111658/0000/",			// 11 PbPb MB4 AOD  |eta|<1, calo, no eff, 0.3 < pt_ref < 3.0
 	"NA",												// 12 PbPb MB1,2,3,4 8+9+10+11 AOD  |eta|<1, calo, no eff, 0.3 < pt_ref < 3.0
+	"../PbPb2015/HIHardProbes/crab_HIHardProbes_Skim_cumu_v8/160222_213524/0000/",			// 13 PbPb HP  AOD  |eta|<1, calo, no eff, 1.0 < pt_ref < 3.0, eff PbPb_dijet_TT_5TeV_v2.root
 };
 
 char const * ftxt[] = {
@@ -29,6 +30,7 @@ char const * ftxt[] = {
 	"txt/HIMB3_v2/",						// 10
 	"txt/HIMB4_v3/",						// 11
 	"txt/HIMB_v1/",							// 12
+	"txt/HIHardProbe_v8/",						// 13
 };
 
 #include <TChain.h>
