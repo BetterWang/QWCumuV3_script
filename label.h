b0b5484ba0b868d49d882fba7520254789fec745
Quan Wang <BetterWang@gmail.com>
2016-09-05 16:43:01 +0200

    [+] lable

--- label.h
+++ label.h
@@ -3,12 +3,14 @@ char const * fname[] = {
 	"test/",			// 0
 	"../PbPb2015_cumu/HIMinimumBias5/crab_HIMB5_ppReco_eff_2part_v2/160829_145024/0000/",			// 1 ppReco rfp 1 < pT < 3
 	"../PbPb2015_cumu/HIMinimumBias3/crab_HIMB3_Reco_eff_2part_v1/160831_001948/0000/",			// 2 Reco rfp 1 < pT < 3
+	"../PbPb2015_cumu/HIMinimumBias5/crab_HIMB5_ppReco_eff_2part_v4/160901_011319/0000/",			// 3 ppReco rfp 0.3 < pT < 3
 };
 
 char const * ftxt[] = {
 	"txt/test/",							// 0
 	"txt/HIMB5_ppReco_eff_2part/",					// 1
 	"txt/HIMB3_Reco_eff_2part/",					// 2
+	"txt/HIMB5_ppReco_eff_2part_v4/",				// 3
 };
 
 #include <TChain.h>
