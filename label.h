7edab49a040890400c609efab639457feff9edfd
Quan Wang <BetterWang@gmail.com>
2017-11-09 22:16:16 -0600

    [+] label

--- label.h
+++ label.h
@@ -1,10 +1,12 @@
 
 char const * fname[] = {
 	"test/",			// 0
+	"../XeXe/HIMinimumBias1/crab_XeXe1_CumuV3_eff_Cent_v1/171109_190654/0000/",			// 1 XeXe Cent
 };
 
 char const * ftxt[] = {
 	"txt/test/",							// 0
+	"txt/XeXe_MB1_Cent",						// 1
 };
 
 #include <TChain.h>
