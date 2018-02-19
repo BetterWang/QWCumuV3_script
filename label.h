8a0a94673efc80ddcda26622d4734e539f109026
Quan Wang <BetterWang@gmail.com>
2018-02-19 23:22:04 +0100

    [+] label

--- label.h
+++ label.h
@@ -22,6 +22,91 @@ char const * fname[] = {
 	"../XeXe/HIMinimumBias19/crab_XeXe19_CumuV3_eff_Cent_v4/180214_220323/0000/",			// 19 XeXe Cent
 	"../XeXe/HIMinimumBias20/crab_XeXe20_CumuV3_eff_Cent_v4/180214_220346/0000/",			// 20 XeXe Cent
 	"NA",			// 21 XeXe Cent
+	"../XeXe/HIMinimumBias1/crab_XeXe1_CumuV3_eff_Cent_sysTight_v1/180215_185240/0000/",			// 22 XeXe1 Cent sysTight
+	"../XeXe/HIMinimumBias2/crab_XeXe2_CumuV3_eff_Cent_sysTight_v1/180215_185254/0000/",			// 23 XeXe2 Cent sysTight
+	"../XeXe/HIMinimumBias3/crab_XeXe3_CumuV3_eff_Cent_sysTight_v1/180215_185308/0000/",			// 24 XeXe3 Cent sysTight
+	"../XeXe/HIMinimumBias4/crab_XeXe4_CumuV3_eff_Cent_sysTight_v1/180215_185322/0000/",			// 25 XeXe4 Cent sysTight
+	"../XeXe/HIMinimumBias5/crab_XeXe5_CumuV3_eff_Cent_sysTight_v1/180215_185341/0000/",			// 26 XeXe5 Cent sysTight
+	"../XeXe/HIMinimumBias6/crab_XeXe6_CumuV3_eff_Cent_sysTight_v1/180215_185404/0000/",			// 27 XeXe6 Cent sysTight
+	"../XeXe/HIMinimumBias7/crab_XeXe7_CumuV3_eff_Cent_sysTight_v1/180215_185425/0000/",			// 28 XeXe7 Cent sysTight
+	"../XeXe/HIMinimumBias8/crab_XeXe8_CumuV3_eff_Cent_sysTight_v1/180215_185444/0000/",			// 29 XeXe8 Cent sysTight
+	"../XeXe/HIMinimumBias9/crab_XeXe9_CumuV3_eff_Cent_sysTight_v1/180215_185459/0000/",			// 30 XeXe9 Cent sysTight
+	"../XeXe/HIMinimumBias10/crab_XeXe10_CumuV3_eff_Cent_sysTight_v1/180215_185513/0000/",			// 31 XeXe10 Cent sysTight
+	"../XeXe/HIMinimumBias11/crab_XeXe11_CumuV3_eff_Cent_sysTight_v1/180215_185527/0000/",			// 32 XeXe11 Cent sysTight
+	"../XeXe/HIMinimumBias12/crab_XeXe12_CumuV3_eff_Cent_sysTight_v1/180215_185545/0000/",			// 33 XeXe12 Cent sysTight
+	"../XeXe/HIMinimumBias13/crab_XeXe13_CumuV3_eff_Cent_sysTight_v1/180215_185604/0000/",			// 34 XeXe13 Cent sysTight
+	"../XeXe/HIMinimumBias14/crab_XeXe14_CumuV3_eff_Cent_sysTight_v1/180215_185627/0000/",			// 35 XeXe14 Cent sysTight
+	"../XeXe/HIMinimumBias15/crab_XeXe15_CumuV3_eff_Cent_sysTight_v1/180215_185649/0000/",			// 36 XeXe15 Cent sysTight
+	"../XeXe/HIMinimumBias16/crab_XeXe16_CumuV3_eff_Cent_sysTight_v1/180215_185705/0000/",			// 37 XeXe16 Cent sysTight
+	"../XeXe/HIMinimumBias17/crab_XeXe17_CumuV3_eff_Cent_sysTight_v1/180215_185719/0000/",			// 38 XeXe17 Cent sysTight
+	"../XeXe/HIMinimumBias18/crab_XeXe18_CumuV3_eff_Cent_sysTight_v1/180215_185734/0000/",			// 39 XeXe18 Cent sysTight
+	"../XeXe/HIMinimumBias19/crab_XeXe19_CumuV3_eff_Cent_sysTight_v1/180215_185751/0000/",			// 40 XeXe19 Cent sysTight
+	"../XeXe/HIMinimumBias20/crab_XeXe20_CumuV3_eff_Cent_sysTight_v1/180215_185805/0000/",			// 41 XeXe20 Cent sysTight
+	"../XeXe/HIMinimumBias1/crab_XeXe1_CumuV3_eff_Cent_sysLoose_v1/180215_203558/0000/",			// 42 XeXe1  Cent sysLoose
+	"../XeXe/HIMinimumBias2/crab_XeXe2_CumuV3_eff_Cent_sysLoose_v1/180215_203614/0000/",			// 43 XeXe2  Cent sysLoose
+	"../XeXe/HIMinimumBias3/crab_XeXe3_CumuV3_eff_Cent_sysLoose_v1/180215_203636/0000/",			// 44 XeXe3  Cent sysLoose
+	"../XeXe/HIMinimumBias4/crab_XeXe4_CumuV3_eff_Cent_sysLoose_v1/180215_203658/0000/",			// 45 XeXe4  Cent sysLoose
+	"../XeXe/HIMinimumBias5/crab_XeXe5_CumuV3_eff_Cent_sysLoose_v1/180215_203715/0000/",			// 46 XeXe5  Cent sysLoose
+	"../XeXe/HIMinimumBias6/crab_XeXe6_CumuV3_eff_Cent_sysLoose_v1/180215_203738/0000/",			// 47 XeXe6  Cent sysLoose
+	"../XeXe/HIMinimumBias7/crab_XeXe7_CumuV3_eff_Cent_sysLoose_v1/180215_203801/0000/",			// 48 XeXe7  Cent sysLoose
+	"../XeXe/HIMinimumBias8/crab_XeXe8_CumuV3_eff_Cent_sysLoose_v1/180215_203815/0000/",			// 49 XeXe8  Cent sysLoose
+	"../XeXe/HIMinimumBias9/crab_XeXe9_CumuV3_eff_Cent_sysLoose_v1/180215_203831/0000/",			// 50 XeXe9  Cent sysLoose
+	"../XeXe/HIMinimumBias10/crab_XeXe10_CumuV3_eff_Cent_sysLoose_v1/180215_203845/0000/",			// 51 XeXe10 Cent sysLoose
+	"../XeXe/HIMinimumBias11/crab_XeXe11_CumuV3_eff_Cent_sysLoose_v1/180215_203858/0000/",			// 52 XeXe11 Cent sysLoose
+	"../XeXe/HIMinimumBias12/crab_XeXe12_CumuV3_eff_Cent_sysLoose_v1/180215_203911/0000/",			// 53 XeXe12 Cent sysLoose
+	"../XeXe/HIMinimumBias13/crab_XeXe13_CumuV3_eff_Cent_sysLoose_v1/180215_203924/0000/",			// 54 XeXe13 Cent sysLoose
+	"../XeXe/HIMinimumBias14/crab_XeXe14_CumuV3_eff_Cent_sysLoose_v1/180215_203937/0000/",			// 55 XeXe14 Cent sysLoose
+	"../XeXe/HIMinimumBias15/crab_XeXe15_CumuV3_eff_Cent_sysLoose_v1/180215_203951/0000/",			// 56 XeXe15 Cent sysLoose
+	"../XeXe/HIMinimumBias16/crab_XeXe16_CumuV3_eff_Cent_sysLoose_v1/180215_204005/0000/",			// 57 XeXe16 Cent sysLoose
+	"../XeXe/HIMinimumBias17/crab_XeXe17_CumuV3_eff_Cent_sysLoose_v1/180215_204019/0000/",			// 58 XeXe17 Cent sysLoose
+	"../XeXe/HIMinimumBias18/crab_XeXe18_CumuV3_eff_Cent_sysLoose_v1/180215_204033/0000/",			// 59 XeXe18 Cent sysLoose
+	"../XeXe/HIMinimumBias19/crab_XeXe19_CumuV3_eff_Cent_sysLoose_v1/180215_204048/0000/",			// 60 XeXe19 Cent sysLoose
+	"../XeXe/HIMinimumBias20/crab_XeXe20_CumuV3_eff_Cent_sysLoose_v1/180215_204102/0000/",			// 61 XeXe20 Cent sysLoose
+	"../XeXe/HIMinimumBias1/crab_XeXe1_CumuV3_eff_Cent_sysNarrow_v1/180217_210554/0000/",			// 62 XeXe1  Cent sysNarrow
+	"../XeXe/HIMinimumBias2/crab_XeXe2_CumuV3_eff_Cent_sysNarrow_v1/180217_210606/0000/",			// 63 XeXe2  Cent sysNarrow
+	"../XeXe/HIMinimumBias3/crab_XeXe3_CumuV3_eff_Cent_sysNarrow_v1/180217_210619/0000/",			// 64 XeXe3  Cent sysNarrow
+	"../XeXe/HIMinimumBias4/crab_XeXe4_CumuV3_eff_Cent_sysNarrow_v1/180217_210631/0000/",			// 65 XeXe4  Cent sysNarrow
+	"../XeXe/HIMinimumBias5/crab_XeXe5_CumuV3_eff_Cent_sysNarrow_v1/180217_210644/0000/",			// 66 XeXe5  Cent sysNarrow
+	"../XeXe/HIMinimumBias6/crab_XeXe6_CumuV3_eff_Cent_sysNarrow_v1/180217_210656/0000/",			// 67 XeXe6  Cent sysNarrow
+	"../XeXe/HIMinimumBias7/crab_XeXe7_CumuV3_eff_Cent_sysNarrow_v1/180217_210708/0000/",			// 68 XeXe7  Cent sysNarrow
+	"../XeXe/HIMinimumBias8/crab_XeXe8_CumuV3_eff_Cent_sysNarrow_v1/180217_210720/0000/",			// 69 XeXe8  Cent sysNarrow
+	"../XeXe/HIMinimumBias9/crab_XeXe9_CumuV3_eff_Cent_sysNarrow_v1/180217_210733/0000/",			// 70 XeXe9  Cent sysNarrow
+	"../XeXe/HIMinimumBias10/crab_XeXe10_CumuV3_eff_Cent_sysNarrow_v1/180217_210746/0000/",			// 71 XeXe10 Cent sysNarrow
+	"../XeXe/HIMinimumBias11/crab_XeXe11_CumuV3_eff_Cent_sysNarrow_v1/180217_210758/0000/",			// 72 XeXe11 Cent sysNarrow
+	"../XeXe/HIMinimumBias12/crab_XeXe12_CumuV3_eff_Cent_sysNarrow_v1/180217_210811/0000/",			// 73 XeXe12 Cent sysNarrow
+	"../XeXe/HIMinimumBias13/crab_XeXe13_CumuV3_eff_Cent_sysNarrow_v1/180217_210823/0000/",			// 74 XeXe13 Cent sysNarrow
+	"../XeXe/HIMinimumBias14/crab_XeXe14_CumuV3_eff_Cent_sysNarrow_v1/180217_210836/0000/",			// 75 XeXe14 Cent sysNarrow
+	"../XeXe/HIMinimumBias15/crab_XeXe15_CumuV3_eff_Cent_sysNarrow_v1/180217_210848/0000/",			// 76 XeXe15 Cent sysNarrow
+	"../XeXe/HIMinimumBias16/crab_XeXe16_CumuV3_eff_Cent_sysNarrow_v1/180217_210900/0000/",			// 77 XeXe16 Cent sysNarrow
+	"../XeXe/HIMinimumBias17/crab_XeXe17_CumuV3_eff_Cent_sysNarrow_v1/180217_210913/0000/",			// 78 XeXe17 Cent sysNarrow
+	"../XeXe/HIMinimumBias18/crab_XeXe18_CumuV3_eff_Cent_sysNarrow_v1/180217_210925/0000/",			// 79 XeXe18 Cent sysNarrow
+	"../XeXe/HIMinimumBias19/crab_XeXe19_CumuV3_eff_Cent_sysNarrow_v1/180217_210937/0000/",			// 80 XeXe19 Cent sysNarrow
+	"../XeXe/HIMinimumBias20/crab_XeXe20_CumuV3_eff_Cent_sysNarrow_v1/180217_210950/0000/",			// 81 XeXe20 Cent sysNarrow
+	"../XeXe/HIMinimumBias1/crab_XeXe1_CumuV3_eff_Cent_sysWide_v1/180218_073830/0000/",			// 82 XeXe1  Cent sysWide
+	"../XeXe/HIMinimumBias2/crab_XeXe2_CumuV3_eff_Cent_sysWide_v1/180218_073049/0000/",			// 83 XeXe2  Cent sysWide
+	"../XeXe/HIMinimumBias3/crab_XeXe3_CumuV3_eff_Cent_sysWide_v1/180219_153409/0000/",			// 84 XeXe3  Cent sysWide
+	"../XeXe/HIMinimumBias4/crab_XeXe4_CumuV3_eff_Cent_sysWide_v1/180218_073241/0000/",			// 85 XeXe4  Cent sysWide
+	"../XeXe/HIMinimumBias5/crab_XeXe5_CumuV3_eff_Cent_sysWide_v1/180218_073255/0000/",			// 86 XeXe5  Cent sysWide
+	"../XeXe/HIMinimumBias6/crab_XeXe6_CumuV3_eff_Cent_sysWide_v1/180218_073308/0000/",			// 87 XeXe6  Cent sysWide
+	"../XeXe/HIMinimumBias7/crab_XeXe7_CumuV3_eff_Cent_sysWide_v1/180218_073324/0000/",			// 88 XeXe7  Cent sysWide
+	"../XeXe/HIMinimumBias8/crab_XeXe8_CumuV3_eff_Cent_sysWide_v1/180218_073341/0000/",			// 89 XeXe8  Cent sysWide
+	"../XeXe/HIMinimumBias9/crab_XeXe9_CumuV3_eff_Cent_sysWide_v1/180218_073354/0000/",			// 90 XeXe9  Cent sysWide
+	"../XeXe/HIMinimumBias10/crab_XeXe10_CumuV3_eff_Cent_sysWide_v1/180218_073409/0000/",			// 91 XeXe10 Cent sysWide
+	"../XeXe/HIMinimumBias11/crab_XeXe11_CumuV3_eff_Cent_sysWide_v1/180218_073424/0000/",			// 92 XeXe11 Cent sysWide
+	"../XeXe/HIMinimumBias12/crab_XeXe12_CumuV3_eff_Cent_sysWide_v1/180218_073438/0000/",			// 93 XeXe12 Cent sysWide
+	"../XeXe/HIMinimumBias13/crab_XeXe13_CumuV3_eff_Cent_sysWide_v1/180218_073452/0000/",			// 94 XeXe13 Cent sysWide
+	"../XeXe/HIMinimumBias14/crab_XeXe14_CumuV3_eff_Cent_sysWide_v1/180218_073506/0000/",			// 95 XeXe14 Cent sysWide
+	"../XeXe/HIMinimumBias15/crab_XeXe15_CumuV3_eff_Cent_sysWide_v1/180218_073520/0000/",			// 96 XeXe15 Cent sysWide
+	"../XeXe/HIMinimumBias16/crab_XeXe16_CumuV3_eff_Cent_sysWide_v1/180218_073534/0000/",			// 97 XeXe16 Cent sysWide
+	"../XeXe/HIMinimumBias17/crab_XeXe17_CumuV3_eff_Cent_sysWide_v1/180218_073549/0000/",			// 98 XeXe17 Cent sysWide
+	"../XeXe/HIMinimumBias18/crab_XeXe18_CumuV3_eff_Cent_sysWide_v1/180218_073602/0000/",			// 99 XeXe18 Cent sysWide
+	"../XeXe/HIMinimumBias19/crab_XeXe19_CumuV3_eff_Cent_sysWide_v1/180218_073615/0000/",			//100 XeXe19 Cent sysWide
+	"../XeXe/HIMinimumBias20/crab_XeXe20_CumuV3_eff_Cent_sysWide_v1/180218_073629/0000/",			//101 XeXe20 Cent sysWide
+	"NA",			//102 XeXe Cent sysTight
+	"NA",			//103 XeXe Cent sysLoose
+	"NA",			//104 XeXe Cent sysNarrow
+	"NA",			//105 XeXe Cent sysWide
+	"../XeXe/XeXeMinBias_5p44TeV-AMPT-withmelt/crab_XeXe_CumuV3_AMPT_Cent_GEN_v2/180215_162921/0000/",	//106 XeXe Cent AMPT melt
 };
 
 char const * ftxt[] = {
@@ -47,6 +132,91 @@ char const * ftxt[] = {
 	"txt/XeXe_MB19_Cent",						// 19
 	"txt/XeXe_MB20_Cent",						// 20
 	"txt/XeXe_MB_Cent",						// 21
+	"txt/XeXe_MB1_Cent_sysTight",						// 22
+	"txt/XeXe_MB2_Cent_sysTight",						// 23
+	"txt/XeXe_MB3_Cent_sysTight",						// 24
+	"txt/XeXe_MB4_Cent_sysTight",						// 25
+	"txt/XeXe_MB5_Cent_sysTight",						// 26
+	"txt/XeXe_MB6_Cent_sysTight",						// 27
+	"txt/XeXe_MB7_Cent_sysTight",						// 28
+	"txt/XeXe_MB8_Cent_sysTight",						// 29
+	"txt/XeXe_MB9_Cent_sysTight",						// 30
+	"txt/XeXe_MB10_Cent_sysTight",						// 31
+	"txt/XeXe_MB11_Cent_sysTight",						// 32
+	"txt/XeXe_MB12_Cent_sysTight",						// 33
+	"txt/XeXe_MB13_Cent_sysTight",						// 34
+	"txt/XeXe_MB14_Cent_sysTight",						// 35
+	"txt/XeXe_MB15_Cent_sysTight",						// 36
+	"txt/XeXe_MB16_Cent_sysTight",						// 37
+	"txt/XeXe_MB17_Cent_sysTight",						// 38
+	"txt/XeXe_MB18_Cent_sysTight",						// 39
+	"txt/XeXe_MB19_Cent_sysTight",						// 40
+	"txt/XeXe_MB20_Cent_sysTight",						// 41
+	"txt/XeXe_MB1_Cent_sysLoose",						// 42
+	"txt/XeXe_MB2_Cent_sysLoose",						// 43
+	"txt/XeXe_MB3_Cent_sysLoose",						// 44
+	"txt/XeXe_MB4_Cent_sysLoose",						// 45
+	"txt/XeXe_MB5_Cent_sysLoose",						// 46
+	"txt/XeXe_MB6_Cent_sysLoose",						// 47
+	"txt/XeXe_MB7_Cent_sysLoose",						// 48
+	"txt/XeXe_MB8_Cent_sysLoose",						// 49
+	"txt/XeXe_MB9_Cent_sysLoose",						// 50
+	"txt/XeXe_MB10_Cent_sysLoose",						// 51
+	"txt/XeXe_MB11_Cent_sysLoose",						// 52
+	"txt/XeXe_MB12_Cent_sysLoose",						// 53
+	"txt/XeXe_MB13_Cent_sysLoose",						// 54
+	"txt/XeXe_MB14_Cent_sysLoose",						// 55
+	"txt/XeXe_MB15_Cent_sysLoose",						// 56
+	"txt/XeXe_MB16_Cent_sysLoose",						// 57
+	"txt/XeXe_MB17_Cent_sysLoose",						// 58
+	"txt/XeXe_MB18_Cent_sysLoose",						// 59
+	"txt/XeXe_MB19_Cent_sysLoose",						// 60
+	"txt/XeXe_MB20_Cent_sysLoose",						// 61
+	"txt/XeXe_MB1_Cent_sysNarrow",						// 62
+	"txt/XeXe_MB2_Cent_sysNarrow",						// 63
+	"txt/XeXe_MB3_Cent_sysNarrow",						// 64
+	"txt/XeXe_MB4_Cent_sysNarrow",						// 65
+	"txt/XeXe_MB5_Cent_sysNarrow",						// 66
+	"txt/XeXe_MB6_Cent_sysNarrow",						// 67
+	"txt/XeXe_MB7_Cent_sysNarrow",						// 68
+	"txt/XeXe_MB8_Cent_sysNarrow",						// 69
+	"txt/XeXe_MB9_Cent_sysNarrow",						// 70
+	"txt/XeXe_MB10_Cent_sysNarrow",						// 71
+	"txt/XeXe_MB11_Cent_sysNarrow",						// 72
+	"txt/XeXe_MB12_Cent_sysNarrow",						// 73
+	"txt/XeXe_MB13_Cent_sysNarrow",						// 74
+	"txt/XeXe_MB14_Cent_sysNarrow",						// 75
+	"txt/XeXe_MB15_Cent_sysNarrow",						// 76
+	"txt/XeXe_MB16_Cent_sysNarrow",						// 77
+	"txt/XeXe_MB17_Cent_sysNarrow",						// 78
+	"txt/XeXe_MB18_Cent_sysNarrow",						// 79
+	"txt/XeXe_MB19_Cent_sysNarrow",						// 80
+	"txt/XeXe_MB20_Cent_sysNarrow",						// 81
+	"txt/XeXe_MB1_Cent_sysWide",						// 82
+	"txt/XeXe_MB2_Cent_sysWide",						// 83
+	"txt/XeXe_MB3_Cent_sysWide",						// 84
+	"txt/XeXe_MB4_Cent_sysWide",						// 85
+	"txt/XeXe_MB5_Cent_sysWide",						// 86
+	"txt/XeXe_MB6_Cent_sysWide",						// 87
+	"txt/XeXe_MB7_Cent_sysWide",						// 88
+	"txt/XeXe_MB8_Cent_sysWide",						// 89
+	"txt/XeXe_MB9_Cent_sysWide",						// 90
+	"txt/XeXe_MB10_Cent_sysWide",						// 91
+	"txt/XeXe_MB11_Cent_sysWide",						// 92
+	"txt/XeXe_MB12_Cent_sysWide",						// 93
+	"txt/XeXe_MB13_Cent_sysWide",						// 94
+	"txt/XeXe_MB14_Cent_sysWide",						// 95
+	"txt/XeXe_MB15_Cent_sysWide",						// 96
+	"txt/XeXe_MB16_Cent_sysWide",						// 97
+	"txt/XeXe_MB17_Cent_sysWide",						// 98
+	"txt/XeXe_MB18_Cent_sysWide",						// 99
+	"txt/XeXe_MB19_Cent_sysWide",						//100
+	"txt/XeXe_MB20_Cent_sysWide",						//101
+	"txt/XeXe_MB_Cent_sysTight",						//102
+	"txt/XeXe_MB_Cent_sysLoose",						//103
+	"txt/XeXe_MB_Cent_sysNarrow",						//104
+	"txt/XeXe_MB_Cent_sysWide",						//105
+	"txt/XeXe_MB_Cent_AMPT",						//106
 };
 
 #include <TChain.h>
